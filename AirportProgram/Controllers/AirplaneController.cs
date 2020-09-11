ec12a87d9c75ac845a1cd105ee536bf858ab0391
Nesreen <nesreenmhd@gmail.com>
2020-09-11 20:38:59 +0200

    Day_01_airport assignment

---
+++ AirportProgram/Controllers/AirplaneController.cs
@@ -0,0 +1,59 @@
+﻿using AirportProgram.Models;
+using System;
+
+
+namespace AirportProgram.Controllers
+{
+
+    public class AirplaneController
+    {
+        private Airplane airplane;
+        public AirplaneController(Airplane airplane)
+        {
+            this.airplane = airplane;
+           
+        }
+
+        public void loadpassengers(int NoOfPassengers)
+        {
+            if ((airplane.CurrentNoOfPassengers + NoOfPassengers)<= airplane.MaxOfPassengers )
+            {
+                airplane.CurrentNoOfPassengers += NoOfPassengers;
+                Console.WriteLine($"\n\tAirplane {airplane.Identification}" +
+                $" loads {NoOfPassengers} passengers");
+
+            }
+
+            else
+            {
+                Console.WriteLine($"\n\tNot possible reach to the maximum number");
+            }
+               
+        }
+
+        public void Unloadpassengers()
+        {
+
+            Console.WriteLine($"\n\tAirplane {airplane.Identification}" +
+                $" unloads {airplane.CurrentNoOfPassengers} passengers");
+
+        }
+
+        public void TakeOff()
+        {
+            airplane.IsFlying = true;
+            Console.WriteLine($"\n\tPlane {airplane.Identification}" +
+                $" takes off");
+
+        }
+
+        public void Lands()
+        {
+            airplane.IsFlying = false;
+            Console.WriteLine($"\n\tPlane {airplane.Identification}" +
+                $" Lands");
+
+        }
+
+    }
+}
---
+++ AirportProgram/Program.cs
@@ -0,0 +1,31 @@
+﻿using AirportProgram.Controllers;
+using AirportProgram.Models;
+using System;
+
+namespace AirportProgram
+{
+   public  class Program
+    {
+        static void Main(string[] args)
+        {
+            AirplaneController airplaneController1, airplaneController2;
+
+            Airplane airplane1 = new Airplane("ABC123",60,20,false,234.0);
+            Airplane airplane2 = new Airplane("DDD888",70,30,false,555.0);
+
+            airplaneController1 = new AirplaneController(airplane1);
+            airplaneController1.loadpassengers(40);
+            airplaneController1.TakeOff();
+
+            airplaneController2 = new AirplaneController(airplane2);
+            airplaneController2.loadpassengers(23);
+            airplaneController1.Lands();
+            airplaneController1.Unloadpassengers();
+
+            airplaneController2.TakeOff();
+            airplaneController2.Lands();
+            airplaneController2.Unloadpassengers();
+
+        }
+    }
+}
